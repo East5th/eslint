a4aa22bdc3b486143d9b4da8f17c1608bc692889
Pete Corey <pcorey@Petes-MacBook-Pro.local>
2015-07-25 18:52:41 -0700

    package.js fix

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.addFiles('eslint.js');
-  epi.export('eslint', 'server');
+  api.export('eslint', 'server');
 });
 
 Npm.depends({
